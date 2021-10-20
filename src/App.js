2aaad772d43ca0c8bb9b5daf969870d8704c5759
Jason Brian Parker <jasonbrianparker@Jasons-MacBook-Pro.local>
2021-10-20 16:09:45 -0700

    add react project

--- src/App.js
+++ src/App.js
@@ -5,21 +5,32 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <input style={{
+          width: '200px',
+          height: '35px',
+          fontSize: '25px',
+          color: 'red'
+        }}/>
+          <p>
+          
+          Jason Parker's site
         </p>
+        <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/close-up-of-flower-blooming-outdoors-royalty-free-image-739387273-1544039749.jpg" width="300px alt="flower/>
+       
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=5qap5aO4i9A"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          music
         </a>
       </header>
     </div>
-  );
+
+  )
 }
 
 export default App;
+
